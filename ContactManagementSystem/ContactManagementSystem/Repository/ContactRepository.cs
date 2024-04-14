c513d7144f625be0816eefc45afa1b35fdfce5ee
lipcseie <lipcsei.e@gmail.com>
2024-04-14 11:17:26 +0200

    add ContactRepository class

---
+++ ContactManagementSystem/ContactManagementSystem/Repository/ContactRepository.cs
@@ -0,0 +1,33 @@
+﻿using ContactManagementSystem.Models;
+
+namespace ContactManagementSystem.Repository
+{
+    public class ContactRepository : IContactRepository
+    {
+        private readonly ContactsContexts _contacts;
+        public Task AddContact(Contact contact)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteContact(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Contact>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Contact> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateContact(Contact contact)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
